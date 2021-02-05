7e4cd1dbebc798c62e736bafee9b0cb9f773cce7
Marco Borchi <borchimarco@gmail.com>
2021-02-05 23:32:21 +0100

    refactor and mapping all the servo

--- AbortinoLegs.ino
+++ AbortinoLegs.ino
@@ -11,21 +11,29 @@
 #define D6 12
 #define D7 13
 
+// WIP
+// TODO Remap pin to servo
+#define PIN6 6
+#define PIN5 5
+#define PIN9 9
+#define PIN8 8
 class BaseServo{
   public:
     byte pin;
     int bottom_pos;
     int center_pos;
+    int top_pos;
     Servo motor;
     unsigned int delay_time = 50;
 
-    BaseServo(byte pin, int center_pos, int bottom_pos){
+    BaseServo(byte pin, int center_pos, int bottom_pos, int top_pos){
       this->pin = pin;
       this->center_pos = center_pos;
       this->bottom_pos = bottom_pos;
+      this->top_pos = top_pos;
     }
 
-    void set_up(){
+    void setup(){
       motor.attach(pin);
       motor.write(center_pos);
       delay(delay_time);
@@ -39,63 +47,87 @@ class BaseServo{
       motor.write(bottom_pos);
       delay(delay_time);
     }
-};
-
-class LegServo: public BaseServo{
-  private:
-    int top_pos;
-  public:
-    LegServo(byte pin, int center_pos, int bottom_pos, int top_pos):BaseServo(pin, center_pos, bottom_pos){
-      this->pin = pin;
-      this->center_pos = center_pos;
-      this->bottom_pos = bottom_pos;
-      this->top_pos = top_pos;
-    }
 
     void set_top(){
       motor.write(top_pos);
       delay(delay_time);
     }
+
+    void set_angle(int angle){
+      motor.write(angle);
+      delay(delay_time);
+    }
+};
+
+class LegServo: public BaseServo{    
+  public:
+    LegServo(byte pin, int center_pos, int bottom_pos, int top_pos):BaseServo(pin, center_pos, bottom_pos, top_pos){
+      this->pin = pin;
+      this->center_pos = center_pos;
+      this->bottom_pos = bottom_pos;
+      this->top_pos = top_pos;
+    }
 };
 
 class FootServo: public BaseServo{
   public:
-    FootServo(byte pin, int center_pos, int bottom_pos):BaseServo(pin, center_pos, bottom_pos){
+    FootServo(byte pin, int center_pos, int bottom_pos,  int top_pos):BaseServo(pin, center_pos, bottom_pos, top_pos){
       this->pin = pin;
       this->center_pos = center_pos;
       this->bottom_pos = bottom_pos;
+      this->top_pos = top_pos;
     }
 };
 
-FootServo frontFootLeft = FootServo(D7, 0, 100);
-FootServo frontFootRight = FootServo(D6, 180, 100);
-FootServo backFootLeft = FootServo(D5, 0, 100);
-FootServo backFootRight = FootServo(D4, 100, 0);
+// center - bottom -  top
 
-Servo frontLegLeft, frontLegRight, backLegLeft, backLegRight;
+// FOOT SERVO
+FootServo frontFootLeft = FootServo(D7, 100, 180, 0);
+FootServo frontFootRight = FootServo(D6, 100, 0, 100);
+FootServo backFootLeft = FootServo(D5, 0, 100, 0);
+FootServo backFootRight = FootServo(D4, 100, 0, 100);
+
+// LEG SERVO
+LegServo frontLegLeft = LegServo(PIN6, 100, 180, 0);
+LegServo frontLegRight = LegServo(PIN5, 100, 0, 180);
+LegServo backLegLeft = LegServo(PIN9, 100, 180, 0);
+LegServo backLegRight = LegServo(PIN8, 100, 0, 180);
 
 void setup() {
   Serial.begin(115200);
-  
-  frontFootLeft.set_up();
-  frontFootRight.set_up();
-  backFootLeft.set_up();
-  backFootRight.set_up();
+  frontFootLeft.setup();
+  frontLegLeft.setup();
+  frontLegRight.setup();
 }
 
 void loop() {
-    frontFootLeft.set_bottom();
-    frontFootRight.set_bottom();
-    backFootLeft.set_bottom();
-    backFootRight.set_bottom();
+  frontLegRight.set_top();
+  hello();
+  frontLegRight.set_center();
+  delay(5000);
+}
 
-    delay(5000);
-   
-    frontFootLeft.set_center();
-    frontFootRight.set_center();
-    backFootLeft.set_center();
-    backFootRight.set_center();
-
-    delay(3000);
-   
+void hello(){
+  frontLegLeft.set_top();
+  frontFootLeft.set_top();
+  delay(500);
+  frontLegLeft.set_center();
+  delay(500);
+  frontLegLeft.set_top();
+  delay(500);
+  frontFootLeft.set_center();
+  delay(500);
+  frontFootLeft.set_top();
+  delay(500);
+  frontLegLeft.set_center();
+  delay(500);
+  frontLegLeft.set_top();
+  delay(500);
+  frontLegLeft.set_center();
+  delay(500);
+  frontLegLeft.set_top();
+  delay(500);
+  frontLegLeft.set_center();
+  delay(500);
+  frontFootLeft.set_bottom();
 }
